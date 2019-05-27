6e11c5e3bdec009d7f41799f72b8c55d5d344da4
vahurtad <vahurtad@ucsc.edu>
2019-05-26 20:34:50 -0700

    more fixes to mobile/tablet

--- pages/index.js
+++ pages/index.js
@@ -3,33 +3,30 @@ import Link from 'next/link'
 import Head from '../components/head'
 import Nav from '../components/nav'
 import '../static/scss/style.scss';
-import { Grommet, Box, Button, Grid, Text } from "grommet";
+import { Image,ResponsiveContext, Grommet, Box, Button, Grid, Text } from "grommet";
 
 const Home = () => (
   <Grommet full>
-    <Head title="Home" />
+    <Head title="vanessa hurtado" />
     {/* <Nav /> */}
-
-      {/* <div className="box">
-        <div className="shape">
-        </div>
-      </div>
-      <div className="box-bottom">
-        <div className="shape">
-        </div>
-      </div> */}
-      <Grid 
-        fill
-        columns={["medium", "flex"]}
-        rows={["flex"]}
+    <ResponsiveContext.Consumer>
+    {size => (
+      <Grid
+        className="grid-layout"
+        align="end"
+        alignContent="start"
+        columns={['1/3','2/3']}
+        rows={size !== 'small' && { count: 'fill', size: '1/3' }}
         gap="small"
-        areas={[
-                { name: "nav", start: [0, 0], end: [0, 0] },
-                { name: "main", start: [1, 0], end: [1, 0] }
-              ]}>
-        <Box fill="horizontal" gridArea="nav"
-             responsive="true" 
-             border={{ color: 'brand', size: 'large' }}>
+              >
+        <Box 
+        className="first-box"
+          fill="horizontal"
+          direction="row"
+          flex='grow' justify='start' 
+          responsive="true" 
+          border={{ color: 'brand', size: 'large' }}
+          >
 
           <div className="box">
             <div className="shape">
@@ -44,21 +41,46 @@ const Home = () => (
             </h1>
           </div>
 
+          <div className="box-bottom">
+          <div className="shape">
+          </div>
+        </div>
+                    
+          <Image
+            className='bg-img'
+            fit="cover"
+            src="/static/images/vector2.jpg"
+            style={{
+              display:'inline-block',
+              background: 'linear-gradient(0deg,rgba(195,207,206,0.8),rgba(195,207,206,0.8))',
+              position:'relative',
+              display:'block',
+              height:'200px', 
+              width:'auto',
+              
+            }}
+          />
+
+          
         </Box>
+        
+        <Box 
+          responsive="true" 
+          flex='grow' justify='start'
+          border={{ color: 'light-2', size: 'large' }}  >
 
-        <Box gridArea="main" responsive="true" 
-        border={{ color: 'light-2', size: 'large' }}  >
+            <Text className="description">
+                  Highly determined programmer always finding motivation in merging my hobbies with technology.
 
-          <article className="about">
-            <p className="description">
-              Highly determined programmer alwayvs finding motivation in merging my hobbies with technology.
+                  I play video games with friends, and on my free time I enjoy learning about data science and other new technologies. 
+            </Text>
 
-              I play video games with friends, and on my free time I enjoy learning about data science and other new technologies. 
-            </p>
-          </article>
+          </Box>
 
-        </Box>
-      </Grid>  
+        
+        </Grid>
+    )}
+  </ResponsiveContext.Consumer>
   </Grommet>
 )
 
